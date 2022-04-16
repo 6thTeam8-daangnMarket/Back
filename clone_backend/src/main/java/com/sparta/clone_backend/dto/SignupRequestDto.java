90d1b3573b19df33d54a80f681689c8fe08dca5c
langho968 <langho968@gmail.com>
2022-04-16 09:31:10 +0900

    회원가입 / 로그인 JWT 완료

---
+++ clone_backend/src/main/java/com/sparta/clone_backend/dto/SignupRequestDto.java
@@ -0,0 +1,16 @@
+package com.sparta.clone_backend.dto;
+
+
+
+import lombok.Getter;
+
+
+
+@Getter
+public class SignupRequestDto {
+    private String username;
+    private String nickname;
+    private String password;
+    private String passwordCheck;
+
+}
